050b8f16768200f138c1701a61643ea03a242070
Dmitriy <wuwka1@ya.ru>
2023-05-26 05:22:54 +0300

    HW2 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 //import Lesson1.Task1;
 //import Lesson1.Task2;
-import Lesson1.Task3;
+//import Lesson1.Task3;
+//import Lesson2.Task1;
+import Lesson2.Task2;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +11,11 @@ public class Main {
 //        Task1.triangularAndFactorial();
 //        Task2 task2 = new Task2();
 //        Task2.primeNumbers();
-        Task3 task3 = new Task3();
-        Task3.calculator();
+//        Task3 task3 = new Task3();
+//        Task3.calculator();
+//        Task1 task1 = new Task1();
+//        Task1.SQLQueryBuilder();
+        Task2 task2 = new Task2();
+        Task2.JSONParser();
     }
 }
