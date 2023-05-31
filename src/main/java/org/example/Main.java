43b52a6d084d80bdb3b112d854734442401f7b47
Dmitriy <wuwka1@ya.ru>
2023-05-31 08:38:48 +0300

    lesson 3. HW.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,12 @@
 package org.example;
-//import Lesson1.Task1;
-//import Lesson1.Task2;
-//import Lesson1.Task3;
-//import Lesson2.Task1;
-import Lesson2.Task2;
+
+import Lesson3.Task1;
 
 public class Main {
     public static void main(String[] args) {
 //        Task1 task1 = new Task1();
 //        Task1.triangularAndFactorial();
-//        Task2 task2 = new Task2();
-//        Task2.primeNumbers();
-//        Task3 task3 = new Task3();
-//        Task3.calculator();
-//        Task1 task1 = new Task1();
-//        Task1.SQLQueryBuilder();
-        Task2 task2 = new Task2();
-        Task2.JSONParser();
+        Task1 task1 = new Task1();
+        Task1.RandomNumberListOperations();
     }
 }
